707daabd010da8ef1aa35a9066bf5b39ac52ebf6
Mukesh Khod <mukeshkhod0001@gmail.com>
2018-09-18 21:28:10 +0530

    adding translator

---
+++ gesture.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+#cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop) 
+# return a single frame in variable `frame`
+
+
+def frameget(cape):
+	ret,frame = cape.read() 
+	while(True):
+	    cv2.imshow('img1',frame) #display the captured image
+	    if cv2.waitKey(1) & 0xFF == ord('y'): #save on pressing 'y' 
+	        cv2.imwrite('images/c1.png',frame)
+	        cv2.destroyAllWindows()
+	        break
+	
+	cap.release()
+
+for i in range(0,5):
+	cap = cv2.VideoCapture(0)
+	frameget(cap)
